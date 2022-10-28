47277325e0b26e83f6091c93dccef45c5ba9272d
aiexgt <lima14florian14@gmail.com>
2022-10-28 14:37:29 -0600

    Finalización dumb-app

--- src/script.js
+++ src/script.js
@@ -1,3 +1,9 @@
 $("#add").click(()=>{
     $("#exampleModal").modal("show");
 })
+
+function agregar(){
+    $("#filaOculta").removeAttr("hidden");
+    $("#resultadoTotal").html("Q.6,599.00")
+    $("#exampleModal").modal("hide");
+}
